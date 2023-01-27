22bd68728903937b000fe82b9ce3b990cc082ac3
zimvhsvn <sitihazimahhassan@gmail.com>
2023-01-28 01:41:14 +0800

    test commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {MdWork} from 'react-icons/md'
+import {RiServiceFill} from 'react-icons/ri'
+import {MdAlternateEmail} from 'react-icons/md'
+import { useState } from 'react'
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('')
+  return (
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdWork /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdAlternateEmail /></a>
+    </nav>
+  )
+}
+
+export default Nav
