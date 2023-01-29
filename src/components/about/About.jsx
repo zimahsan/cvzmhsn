4fab9691770aac058c398a16fbe921a2804453f5
zimvhsvn <sitihazimahhassan@gmail.com>
2023-01-30 00:46:35 +0800

    about jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias totam quos quasi, dicta veniam praesentium maxime, ut dignissimos vitae quidem, voluptatem earum. Dolore unde enim ullam quod beatae! Quod, ad?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
